fdfa3c5459e3297da8b1070d2aa9358eccf930ed
Juan Ochoa <loljuanchogaymer@gmail.com>
2023-11-25 16:36:42 -0500

    Estructura realizada
    Ahi esta la estructura que necesitamos y con la que nos vamos a guiar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
-export default function App() {
+import React from 'react';
+import Header from './components/Header/Header';
+import Sidebar from './components/Sidebar/Sidebar';
+import Home from './pages/Home';
+
+const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+    <div>
+      <Header />
+      <div className="flex">
+        <Sidebar />
+        <Home />
+      </div>
+    </div>
+  );
+};
+
+export default App;
