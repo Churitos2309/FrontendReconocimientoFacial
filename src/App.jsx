88e4882df7b2b3421ae6fcb54fb41b2a57e550b6
Juan Ochoa <loljuanchogaymer@gmail.com>
2023-11-25 21:37:00 -0500

    Links de toda la pagina
    Ya se puede trabajar con los link que tengo dentro del app

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,27 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+
 import Header from './components/Header/Header';
-import Sidebar from './components/Sidebar/Sidebar';
+import Sidebar from './Components/Sidebar/Sidebar';
 import Home from './pages/Home';
-import RegistroForm from './pages/register'
-import LoginForm from './pages/login'
+import RegistroForm from './pages/register';
+import LoginForm from './pages/login';
 
 const App = () => {
   return (
     <div className='flex flex-col h-screen overflow-hidden'>
       <Header />
-      <div className="flex">
+      <div className="flex min-h-screen">
         <Sidebar />
         <div className='flex-grow flex flex-col overflow-hidden'>
           <Home />
         </div>
-        <div>
-          <RegistroForm />
-        </div>
-        <div>
-          <LoginForm />
-        </div>
+        <Routes>
+          <Route path='/Login' element={<LoginForm />} />
+          <Route path='/Register' element={<RegistroForm />} />
+        </Routes>
+        
       </div>
     </div>
   );
--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,34 +1,50 @@
 import React from 'react';
 import { FaHome, FaTh, FaAddressBook, FaHistory } from 'react-icons/fa';
 import { FaLaptopFile } from "react-icons/fa6";
+import { Link } from 'react-router-dom';
 
 const Sidebar = () => {
   return (
-    <aside className="bg-gray-900 w-1/5 p-4 h-screen">
+    <aside className="bg-[#262E30] w-1/5 p-4 h-screen">
       <div className="flex items-center justify-center mb-8">
         <div className="bg-white h-1 w-3/4 rounded-full"></div>
       </div>
       <div className="flex flex-col space-y-4">
+        <Link to='/'>
         <div className="flex items-center space-x-4">
           <FaHome className="text-white" />
           <span className="text-white">Inicio</span>
         </div>
+        </Link>
+
+
+        <Link to='/otraopcion'>
         <div className="flex items-center space-x-4">
           <FaTh className="text-white" />
           <span className="text-white">Otra opción</span>
         </div>
+        </Link>
+
+        <Link to='registro'>
         <div className="flex items-center space-x-4">
           <FaAddressBook className="text-white" />
           <span className="text-white">Registro</span>
         </div>
+        </Link>
+
+        <Link to='/objetosregistrados'>
         <div className="flex items-center space-x-4">
           <FaLaptopFile className="text-white" />
           <span className="text-white">Objetos Registrados</span>
         </div>
+        </Link>
+        
+        <Link to='/historial'>
         <div className="flex items-center space-x-4">
           <FaHistory className="text-white" />
           <span className="text-white">Historial</span>
         </div>
+        </Link>
       </div>
       <div className="mt-auto ">
         <div className="flex items-center justify-center mt-8">
