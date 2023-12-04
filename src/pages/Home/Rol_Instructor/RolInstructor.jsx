cf2970207147ce0bc68acbf9711c246d9cc30dfc
Onixdrew <andrewmosquera1811@gmail.com>
2023-12-04 15:16:42 -0500

    cambios responsivos en la tabla

--- src/pages/Home/Rol_Instructor/RolInstructor.jsx
+++ src/pages/Home/Rol_Instructor/RolInstructor.jsx
@@ -10,7 +10,7 @@ const RolInstructor = () => {
     <>
       <div className="rol_ints">
         <NameProgram></NameProgram>
-        <div className="rol_ints2">
+        <div className="rol_ints2 sm:w-20">
           <h1 className="textof">Fichas asignadas</h1>
           <FichasIntructor />
         </div>
