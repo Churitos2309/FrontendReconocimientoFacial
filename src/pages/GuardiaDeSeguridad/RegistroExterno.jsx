650ea52b92a9a87e8bc02616c1380a4d611a8324
JesusPaladinez <jesuspaladinez18@gmail.com>
2023-12-06 12:22:51 -0500

    Actualización del formulario del registro externo

--- src/pages/GuardiaDeSeguridad/RegistroExterno.jsx
+++ src/pages/GuardiaDeSeguridad/RegistroExterno.jsx
@@ -24,7 +24,13 @@ const RegistroExterno = () => {
                     <label htmlFor="repeat-password" className="block mb-2 text-sm font-medium text-gray-900">Empresa, institución o particular:</label>
                     <input type="text" id="empresa" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required/>
                 </div>
-                <button type="submit" className="text-white bg-gray-800 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Aceptar   </button>
+                <div>
+                    <form className="max-w-sm mx-auto mb-5">
+                        <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900">Diligencia a realizar</label>
+                        <textarea id="message" rows="4" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"></textarea>
+                    </form>
+                </div>
+                <button type="submit" className="text-white bg-gray-800 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mb-5">Aceptar   </button>
             </form>
         </div>
     );
